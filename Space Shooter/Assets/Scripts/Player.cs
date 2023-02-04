3190872bc9e54339731e324e9bc3ee620fbdb080
kzyymmt53 <kzyymmt53@gmail.com>
2023-02-05 02:33:36 +0900

    リファクタリング

--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -19,7 +19,10 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
     }
 }
