44df5a311bb7489189402681f66db84277c3e9cc
kzyymmt53 <kzyymmt53@gmail.com>
2023-02-06 00:58:56 +0900

    レーザー

---
+++ Space Shooter/Assets/Scripts/Laser.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float _speed = 8.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+    }
+}
