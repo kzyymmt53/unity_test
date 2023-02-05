e9f1f7a98979529d4ee064485a4c1e80e6102dd8
kzyymmt53 <kzyymmt53@gmail.com>
2023-02-06 01:14:30 +0900

    Time.timeでゲームをやっている時間を取得できるのでこれを利用してクールダウンタイムを作る

--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -9,6 +9,10 @@ public class Player : MonoBehaviour
     private float _speed = 3.5f;
     [SerializeField]
     private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _canFire = -1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +25,9 @@ public class Player : MonoBehaviour
     {
         CalculateMovement();
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
+            _canFire = Time.time + _fireRate;
             Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
         }
     }
