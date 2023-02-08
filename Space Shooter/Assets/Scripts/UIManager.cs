eab07becf7eebfd92cff0e73d93d4120a23b02ef
kzyymmt53 <kzyymmt53@gmail.com>
2023-02-09 01:56:33 +0900

    リスタート

--- Space Shooter/Assets/Scripts/UIManager.cs
+++ Space Shooter/Assets/Scripts/UIManager.cs
@@ -12,10 +12,21 @@ public class UIManager : MonoBehaviour
 
     [SerializeField]
     private Sprite[] _livesSprite;
+
+    [SerializeField]
+    private Text _gameOverText;
+
+    [SerializeField]
+    private Text _restartText;
+
+    [SerializeField]
+    private GameManager _gameManager;
     // Start is called before the first frame update
     void Start()
     {
         _scoreText.text = "Scpre: " + 0;
+        _gameOverText.gameObject.SetActive(false);
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
         
     }
 
@@ -25,5 +36,24 @@ public class UIManager : MonoBehaviour
 
     public void UpdateLives(int currentLives){
         _LivesImg.sprite = _livesSprite[currentLives]; 
+        if(currentLives == 0){
+             GameOverSequence();
+        }
+    }
+
+    void GameOverSequence(){
+        _gameManager.GameOver();
+        _gameOverText.gameObject.SetActive(true);
+        _restartText.gameObject.SetActive(true);
+        StartCoroutine(GameOverFlickerRoutine());
+    }
+
+    IEnumerator GameOverFlickerRoutine() {
+        while(true){
+            _gameOverText.text = "GAME OVER";
+            yield return new WaitForSeconds(0.5f);
+            _gameOverText.text = "";
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
