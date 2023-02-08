ec1ec8c82773719db0db5f1a4c1bce22988b8747
kzyymmt53 <kzyymmt53@gmail.com>
2023-02-09 02:17:03 +0900

    title画面

--- Space Shooter/Assets/Scripts/GameManager.cs
+++ Space Shooter/Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
 
     private void Update() {
         if(Input.GetKeyDown(KeyCode.R) && _isGameOver == true){
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(1);
         }
     }
 
