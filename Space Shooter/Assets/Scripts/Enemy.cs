cdd6adebe0dab503549288e974534c1c732e2c57
kzyymmt53 <kzyymmt53@gmail.com>
2023-02-13 00:57:33 +0900

    enemy爆発

--- Space Shooter/Assets/Scripts/Enemy.cs
+++ Space Shooter/Assets/Scripts/Enemy.cs
@@ -8,10 +8,22 @@ public class Enemy : MonoBehaviour
     private float _speed = 4.0f;
 
     private Player _player;
+    private Animator _anim;
     // Start is called before the first frame update
     void Start()
     {
         _player = GameObject.Find("Player").GetComponent<Player>();
+        if(_player == null){
+            Debug.LogError("player is null");
+        }
+
+        _anim = GetComponent<Animator>();
+
+        if(_anim == null){
+            Debug.LogError("Animator null");
+        }
+
+       
     }
 
     // Update is called once per frame
@@ -34,7 +46,9 @@ public class Enemy : MonoBehaviour
             if(player != null){
                 player.Damage();
             }
-            Destroy(this.gameObject);
+             _anim.SetTrigger("OnEnemyDeath");
+             _speed = 0;
+            Destroy(this.gameObject, 2.8f);
         }
 
         if(other.tag == "Laser")
@@ -43,7 +57,9 @@ public class Enemy : MonoBehaviour
             if(_player != null){
                 _player.AddScore(10);
             }
-            Destroy(this.gameObject);
+            _anim.SetTrigger("OnEnemyDeath");
+            _speed = 0;
+            Destroy(this.gameObject, 2.8f);
         }
     }
 }
